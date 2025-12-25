cf04e3c09aa02e45bdf8ae61a0d8c1c766481d98
Wizard-555 <kishankishore55555@gmail.com>
2025-12-25 21:24:09 +0530

    Learning Oops and classes in JavaScript

---
+++ classes_and_oops/oop.js
@@ -0,0 +1,40 @@
+// Object literals
+const user = {
+    username: "Kishan",
+    loginCount: 5,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        console.log(`Username: ${this.username}`);
+        console.log(this);       // this -> it will tell us about current context
+    }
+
+}
+console.log(user.username)
+console.log(user.getUserDetails());
+// console.log(this);           // here it will tell about global context (document or Window)
+
+
+
+
+// use of constructor
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this      // we can remove this line also, because retur happend automatically
+}
+// constructor function (or call)
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne);
+console.log(userTwo);
+
+console.log(userOne.constructor);
